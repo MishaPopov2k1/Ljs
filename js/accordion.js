e2d37fdf636be338cad46b3298e364863da23f48
Mikhail <popov.misha.68@mail.ru>
2021-05-19 19:09:55 +0200

    Vers2.0

--- js/accordion.js
+++ js/accordion.js
@@ -3,45 +3,45 @@ document.addEventListener('DOMContentLoaded', () => {
     const featureLinksElems = document.querySelectorAll('.feature__link ');
     const featureSubElems = document.querySelectorAll('.feature-sub');
 
-    let active = featureLinksElems[0]
-    for (let i = 0; i < featureLinksElems.length; i++) {
-        featureLinksElems[i].addEventListener('click', () => {
-
-            for (let j = 0; j < featureSubElems.length; j++) {
-                featureSubElems[j].classList.add('hidden');
-            }
-
-            for (let j = 0; j < featureLinksElems.length; j++) {
-                featureLinksElems[j].classList.remove('feature__link_active');
-            }
-
-            if (featureLinksElems[i] != active) {
-                featureSubElems[i].classList.toggle('hidden');
-                featureLinksElems[i].classList.toggle('feature__link_active');
-                active = featureLinksElems[i];
-            } else active = null
-        })
-    }
-
     // let active = featureLinksElems[0]
-    // featureLinksElems.forEach((btn, index) => {
-    //     btn.addEventListener('click', (e) => {
+    // for (let i = 0; i < featureLinksElems.length; i++) {
+    //     featureLinksElems[i].addEventListener('click', () => {
 
-    //         featureSubElems.forEach((featureSubElem) => {
-    //             featureSubElem.classList.add('hidden')
-    //         })
-    //         featureLinksElems.forEach((featureLinksElem) => {
-    //             featureLinksElem.classList.remove('feature__link_active')
-    //         })
+    //         for (let j = 0; j < featureSubElems.length; j++) {
+    //             featureSubElems[j].classList.add('hidden');
+    //         }
 
-    //         if (btn != active) {
-    //             featureSubElems[index].classList.toggle('hidden');
-    //             btn.classList.toggle('feature__link_active');
-    //             active = btn;
+    //         for (let j = 0; j < featureLinksElems.length; j++) {
+    //             featureLinksElems[j].classList.remove('feature__link_active');
+    //         }
+
+    //         if (featureLinksElems[i] != active) {
+    //             featureSubElems[i].classList.toggle('hidden');
+    //             featureLinksElems[i].classList.toggle('feature__link_active');
+    //             active = featureLinksElems[i];
     //         } else active = null
-
     //     })
-    // })
+    // }
+
+    let active = featureLinksElems[0]
+    featureLinksElems.forEach((btn, index) => {
+        btn.addEventListener('click', (e) => {
+
+            featureSubElems.forEach((featureSubElem) => {
+                featureSubElem.classList.add('hidden')
+            })
+            featureLinksElems.forEach((featureLinksElem) => {
+                featureLinksElem.classList.remove('feature__link_active')
+            })
+
+            if (btn != active) {
+                featureSubElems[index].classList.toggle('hidden');
+                btn.classList.toggle('feature__link_active');
+                active = btn;
+            } else active = null
+
+        })
+    })
 
 })
 
---
+++ js/smothScroll.js
@@ -0,0 +1,11 @@
+const smothScroll = document.querySelectorAll('a[href^="#"]:not(a[href = "#"])')
+smothScroll.forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault()
+        const id = link.getAttribute('href').substring(1)
+        console.log(id)
+        document.getElementById(id).scrollIntoView({
+            behavior: 'smooth'
+        });
+    })
+})
